1d2bd53421a38e176465e6bf8ae15e21a6f220ea
daniel commins <daniel.commins@techelevator.com>
2024-02-12 22:16:51 -0800

    m1w7 lecture code final

---
+++ module-1/week-7/lecture-code-final/src/test/java/UnitTestingExamplesTest2.java
@@ -0,0 +1,18 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UnitTestingExamplesTest2 {
+
+    @Test
+    public void isEven() {
+    }
+
+    @Test
+    public void stringSplosion() {
+    }
+
+    @Test
+    public void stringYak() {
+    }
+}
